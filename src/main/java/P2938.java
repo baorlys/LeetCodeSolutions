fbc7378bf79868690a5fa1cecef4fb7b76869bb3
Ly Gia Bao <69019508+baorlys@users.noreply.github.com>
2024-10-15 15:27:16 +0700

    p2938

---
+++ src/main/java/P2938.java
@@ -0,0 +1,21 @@
+public class P2938 {
+    public static void main(String[] args) {
+        String s = "101";
+        System.out.println(new P2938().minimumSteps(s));
+    }
+
+
+    public long minimumSteps(String s) {
+        long swap = 0;
+        int black = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                swap += black;
+            }
+            else {
+                black++;
+            }
+        }
+        return swap;
+    }
+}
